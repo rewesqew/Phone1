2f4bce337375f4b02019a9c1d9501a60b641bd00
rewesqew <119162080+rewesqew@users.noreply.github.com>
2022-12-20 15:17:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+class Phone:
+  def __init__ (self):
+    self.charters=55
+    self.model="Samsung"
+    self.camera=100
+  def health(self):
+   print("Low Battary") 
+  def messages(self):
+   print("Viber")
+Phone1=Phone()
+Phone1.messages()
