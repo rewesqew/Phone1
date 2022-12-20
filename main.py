fdb6db82bcb1f0a779394069a389a1b514ca8f24
rewesqew <119162080+rewesqew@users.noreply.github.com>
2022-12-20 16:21:03 +0000

    add:TeacherOBJ

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-from random import *
-class Dog1:
+class Human:
   def __init__(self):
-   self.name="BOBIK"
    self.health=100
-   self.eat="meat"
-  def playing(self):
-   print("he is playing")
+   self.brain=100
+   self.name=None
+  def Go_To_School(self):
+    print("They Are Going To School")
+    self.brain+=5
+    self.health-=3
+  def Sleeping(self):
+   print("They Are Sleeping")
+   self.brain+=1
    self.health+=5
-  def eating(self):
-   print("He is Eating")
-   self.health+=15
-  def live(self):
-     live_cube=randint(1,2)
-     if live_cube==1:
-       self.playing()
-     if live_cube==2:
-       self.eating()
-Dog=Dog1()
-for I in range(10): 
-  Dog.live()
+class Teacher (Human):
+  def Talking(self):
+   print("New Topic")
+  def __init__(self):
+   super().__init__()
+   self.every_month_take_a_money=15000
+class Student (Human):
+   def Study(self):
+    print("He Is Studing")
+   def __init__(self): 
+    super().__init__()
+    self.everyday_have_a_homework=True
+
+TeacherOBJ=Teacher()
+StudentOBJ=Student()
+TeacherOBJ.Talking()
+TeacherOBJ.Go_To_School()
+TeacherOBJ.Sleeping()
+print(TeacherOBJ.health)
