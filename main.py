b6cf74943b1c711b931bb3c20bf696bf93a484ea
rewesqew <119162080+rewesqew@users.noreply.github.com>
2022-12-20 15:54:36 +0000

    add dog live

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-class Phone:
-  def __init__ (self):
-    self.charters=55
-    self.model="Samsung"
-    self.camera=100
-  def health(self):
-   print("Low Battary") 
-  def messages(self):
-   print("Viber")
-Phone1=Phone()
-Phone1.messages()
+from random import *
+class Dog1:
+  def __init__(self):
+   self.name="BOBIK"
+   self.health=100
+   self.eat="meat"
+  def playing(self):
+   print("he is playing")
+   self.health+=5
+  def eating(self):
+   print("He is Eating")
+   self.health+=15
+  def live(self):
+     live_cube=randint(1,2)
+     if live_cube==1:
+       self.playing()
+     if live_cube==2:
+       self.eating()
+Dog=Dog1()
+for I in range(10): 
+  Dog.live()
